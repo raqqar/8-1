d9b92494adad75a6fe6d54b24fe768468870b90f
Viacheslav Alekseev <VA_Alekseev@outlook.com>
2022-03-22 21:02:46 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+
+namespace ExportIFC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            IFCExportOptions IFCExportOptions = new IFCExportOptions();
+            string path = "";
+
+
+            
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog(); 
+
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                path = dialog.FileName;
+            }
+            else
+            {
+                return Result.Succeeded;
+            }
+
+            using (var ts = new Transaction(doc, "export dwg"))
+            {
+                ts.Start();
+                doc.Export(path, "ExportIFC", IFCExportOptions);
+                ts.Commit();
+            }
+            TaskDialog.Show("Cообщение", "Экспортирование завершено");
+
+            return Result.Succeeded;
+        }
+    }
+}
